0268c11a206140bd35f04e61fba5167d423db67b
Craig <Craigm@98115.net>
2011-11-06 15:53:20 -0800

    extend collisionUtil to allow building oct-trees

--- collisionUtil.py
+++ collisionUtil.py
@@ -1,4 +1,6 @@
-from panda3d.core import NodePath,CollisionPolygon,CollisionNode,GeomVertexReader,BitMask32
+from panda3d.core import NodePath,CollisionPolygon,CollisionNode,GeomVertexReader,BitMask32,Point3,BoundingSphere
+
+import math
 
 groundMask=BitMask32(0b1)
 
@@ -43,3 +45,101 @@ def rebuildGeomNodesToColPolys (incomingNode,relativeTo=None):
             #n.show()
             
     return parent 
+
+
+def _toCenterBox(b):
+    if isinstance(b,BoundingSphere):
+        return b.getCenter(),b.getRadius()
+    a=b.getMin()
+    b=b.getMax()
+    center=(a+b)/2
+    maxDist=max(max(abs(a[i]-center[i]),abs(b[i]-center[i])) for i in [0,1,2])
+    return center,maxDist
+
+
+
+
+def colTree (incomingNode): 
+    '''
+    assumes all passed nodes are collision nodes with no transforms
+    
+    returns produced oct-tree nodePath
+    ''' 
+    
+    maxLevels=10
+    b=incomingNode.getBounds()
+    maxSize=b.getCenter().length()+b.getRadius()
+    maxSize*=1.1
+    levels=[{} for i in range(maxLevels)]
+    top=NodePath(CollisionNode('colTreeTop'))
+    levels[0][(0,0,0)]=top
+    
+    offset=Point3(maxSize,maxSize,maxSize)
+    
+    def fillNode(level,key):
+        if level==0:
+            assert key==(0,0,0)
+        d=levels[level]
+        cell=d.get(key)
+        if cell is None:
+            cell=NodePath(CollisionNode(str(key)))
+            d[key]=cell
+            if level>0:
+                aboveKey=(key[0]/2,key[1]/2,key[2]/2)
+                aboveCell=fillNode(level-1,aboveKey)
+                cell.reparentTo(aboveCell)
+        return cell
+        
+    for c in incomingNode.findAllMatches('**/+CollisionNode'): 
+        cNode = c.node() 
+        for i in range(cNode.getNumSolids()): 
+            s=cNode.getSolid(i)
+            b=s.getBounds()
+            if not b.isEmpty():
+                center,size=_toCenterBox(b)
+                if size>0:
+                    size=size/maxSize
+                    level=math.log(1/size,2)
+                    level+=3
+                    level=min(level,maxLevels-1)
+                    level=max(level,0)
+                    level=int(math.ceil(level))
+                    
+                    center=center+offset
+                    center=center/(maxSize*2)
+                    
+                    center=center*(2**level)
+                    key=tuple(int(math.floor(center[i])) for i in [0,1,2])
+                    cell=fillNode(level,key).node()
+                    cell.addSolid(s)
+    
+    _mergeCol(top)          
+    return top
+
+def _mergeCol(node):
+    """
+    assumes all passed nodes are collision nodes
+    
+    merges solids for child nodes up to minimize nodes with few solids
+    """
+    mergeCount=10
+    c=node.node()
+    for n in node.getChildren():
+        _mergeCol(n)
+        cn=n.node()
+        removed=False
+        if n.getNumChildren()==0:
+            num=cn.getNumSolids()
+            if num<mergeCount:
+                for i in xrange(num):
+                    c.addSolid(cn.getSolid(i))
+                n.removeNode()
+                removed=True
+        if not removed:
+            numChildren=n.getNumChildren()
+            num=cn.getNumSolids()
+            if num==0 and numChildren==1:
+                nn=n.getChild(0)
+                nn.reparentTo(node)
+                n.removeNode()
+    
